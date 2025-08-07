b65dc666a542cc53eb60cc56949e628493bc1596
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2025-08-07 20:39:54 -0300

    Preparo inicial
    Divisão e tarefas a serem realizadas em foto celular.
    Falta revisar se falta algo e testar a navegação.

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native'; 
+
+import StackNavigation from './stackNavigation';
+
+export default function Navegacao() {
+  return (
+    <NavigationContainer>
+        <StackNavigation />
+    </NavigationContainer>
+  );
+}
+
---
+++ src/screens/EsqSenha/index.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'; 
+
+export default function EsqSenha() {
+    return(
+        <View>
+            <Text>Esqueceu Senha</Text>
+        </View>
+    );
+}
